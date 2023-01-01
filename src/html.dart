efb445b673b2845ab898388f8ae4933fd2879d4e
Dmytro Buryak <dmytro.buryak@gmail.com>
2023-01-01 16:44:00 +0200

    Playing around

--- src/html.dart
+++ src/html.dart
@@ -1,14 +1,15 @@
 import 'dart:html';
 
-void main() {
+main() {
   var button = new Element.tag('button');
   button.text = 'Click me!';
   print("button = ${button}");
   button.onClick.listen((e) {
     var buttons = querySelectorAll('button');
+    print('button click');
     window.alert('You clicked ${buttons.length} buttons!');
   });
   print("body is null: ${document.body == null}");
-  document.body?.children.add(button);
+  document.body!.children.add(button);
   print("button added");
 }
