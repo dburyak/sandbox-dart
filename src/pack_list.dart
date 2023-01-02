6cc398f7d72b8dbd486e677918c033eaf5387812
Dmytro Buryak <dmytro.buryak@gmail.com>
2023-01-02 15:51:50 +0200

    Added more html exercises

--- src/pack_list.dart
+++ src/pack_list.dart
@@ -41,12 +41,12 @@ buildItemsList() {
   itemsDiv.id = 'items-div';
   itemsDiv.style.width = '300px';
   itemsDiv.style.border = '1px solid black';
-  // itemsDiv.innerHtml = '&nbsp;';
   return itemsDiv;
 }
 
-attachAddListeners(ButtonElement addBtn, InputElement input, DivElement itemsList) {
-  final adderFn = (_) {
+attachAddListeners(ButtonElement addBtn, InputElement input,
+    DivElement itemsList) {
+  final addFn = (_) {
     final itemStr = input.value?.trim() ?? '';
     if (itemStr.isEmpty) {
       return;
@@ -54,17 +54,35 @@ attachAddListeners(ButtonElement addBtn, InputElement input, DivElement itemsLis
     itemsList.children.add(buildItem(itemStr));
     input.value = '';
   };
-  addBtn.onClick.listen(adderFn);
+  addBtn.onClick.listen(addFn);
   input.onKeyPress.listen((e) {
     if (e.keyCode == KeyCode.ENTER) {
-      adderFn(e);
+      addFn(e);
     }
   });
 }
 
 buildItem(String itemStr) {
-  final itemDiv = new DivElement();
-  itemDiv.text = itemStr;
-  itemDiv.className = 'item';
-  return itemDiv;
+  final item = new PackItem(itemStr, false);
+  return item.ui;
+}
+
+class PackItem {
+  String name;
+  bool checked;
+  late DivElement _div;
+
+  PackItem(this.name, this.checked) {
+    _div = new DivElement();
+    _div.text = name;
+    _div.className = checked ? 'item checked' : 'item';
+    _div.onClick.listen((_) => _toggle());
+  }
+
+  get ui => _div;
+
+  void _toggle() {
+    checked = !checked;
+    _div.className = checked ? 'item checked' : 'item';
+  }
 }
