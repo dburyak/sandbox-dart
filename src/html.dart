d4ad4c12ce965972f55951f0bd90e2de61caa6f2
Dmytro Buryak <dmytro.buryak@gmail.com>
2023-01-02 10:19:28 +0200

    Playing around

--- src/html.dart
+++ src/html.dart
@@ -4,12 +4,20 @@ main() {
   var button = new Element.tag('button');
   button.text = 'Click me!';
   print("button = ${button}");
+  var numClicks = 0;
+  var status = querySelector("#status")!;
+  status.text = "Click the button!";
+  var history = querySelector("#history")!;
   button.onClick.listen((e) {
     var buttons = querySelectorAll('button');
     print('button click');
     var test = 123 + 245;
     print('test = ${test}');
-    window.alert('You clicked ${buttons.length} buttons!');
+    window.alert('You clicked button ${++numClicks} times!');
+    status.text = "Num clicks: ${numClicks}";
+    final now = DateTime.now();
+    final newHistoryEntry = new Element.html('<div>[${now}] - ${numClicks}</div>');
+    history.children.add(newHistoryEntry);
   });
   print("body is null: ${document.body == null}");
   document.body!.children.add(button);
